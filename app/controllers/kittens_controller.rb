f12dcc4266281e7a5158d57568036c94ef9a4d5b
C4mz0r <aut0x@hotmail.com>
2016-07-17 14:57:38 -0600

    Complete controller and view for index and show

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,30 @@
 class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+  
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def delete
+    
+  end
+  
 end
